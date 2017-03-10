fba333ef9260be206a398e564c16d19a2ac84cff
Prachi Bhardwaj <prachi.bhardwaj@journalism.cuny.edu>
2017-03-10 13:26:38 -0500

    calculator functioning

--- main.js
+++ main.js
@@ -1,61 +1,48 @@
-//first declare counters for total calories and total cost globally and then set to zero.
-var numeralA = 0,
-	numeralB = 0;
-
-
-$(document).ready(function () { //on DOM ready, bind clicks
+$(document).ready(function () {
 	$("#compute").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
+		e.preventDefault();
 
+		$(".answers").addClass(".active");
 
-		$(".numeral").each(function () { //look at each element with the class 'fruit', one by one.
-			//Our variables are all preceeded by a single 'var' statement,
-			//which looks neater but still confines them to the scope of this function.
+	});
+});
 
-			var numeral = $(this), //just like when clicking on classes, we need to know which one of the fruits our code is looking at now.
-				numeralA = parseFloat(numeral.val()), //store the number of pieces of this fruit the user requested
-				numeralB = parseFloat(numeral.val());
+$(document).ready(function () {
+	$("#compute").on("click", function (e) {
+		e.preventDefault();
 
-			//fire the total add function to calculate sum
-			sum(numeralA, numeralB);
+		var a = parseFloat($("#a").val());
+		var b = parseFloat($("#b").val());
 
-			//fire the multiply function to calculate product
-			product(numeralA, numeralB);
+		var sum = add(a, b);
+		var product = multiply(a, b);
+		var quotient = divide(a, b);
+		var difference = subtract(a, b);
 
-			//fire the divide function to calculate quotient
-			quotient(numeralA, numeralB);
+		$("#sum").text(sum);
+		$("#product").text(product);
+		$("#quotient").text(quotient);
+		$("#difference").text(difference);
+		$(".a").text(a);
+		$(".b").text(b);
 
-			//fire the subtract function to calculate difference
-			difference(numeralA, numeralB);
-		}); //end each
-
-		//once we've gotten through all of the fruits, output the results
-		show_results();
 	});
 });
 
 //defining the functions outside of document.ready
 
-function sum(numeralA, numeralB) {
-	var sum = (numeralA + numeralB);
+function add(a, b) {
+	return a + b;
 }
 
-function product(numeralA, numeralB) {
-	var product = (numeralA * numeralB);
+function multiply(a, b) {
+	return a * b;
 }
 
-function quotient(numeralA, numeralB) {
-	var quotient = (numeralA / numeralB);
+function divide(a, b) {
+	return a / b;
 }
 
-function difference(numeralA, numeralB) {
-	var difference = (numeralA - numeralB);
-}
-
-function show_results() {
-	//totalCost and totalCalories are GLOBAL variables. This function can see them no problem!
-	$("#sum").val(sum);
-	$("#product").val(product);
-	$("#quotient").val(quotient);
-	$("#difference").val(difference);
+function subtract(a, b) {
+	return a - b;
 }
