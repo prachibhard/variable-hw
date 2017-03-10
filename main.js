121792676d3cadec1ba426cc0ab36034a64786e6
Prachi Bhardwaj <prachi.bhardwaj@journalism.cuny.edu>
2017-03-10 14:47:41 -0500

    added parentheses back

--- main.js
+++ main.js
@@ -25,17 +25,17 @@ $(document).ready(function () {
 //defining the functions outside of document.ready
 
 function add(a, b) {
-	return a + b;
+	return (a + b);
 }
 
 function multiply(a, b) {
-	return a * b;
+	return (a * b);
 }
 
 function divide(a, b) {
-	return a / b;
+	return (a / b);
 }
 
 function subtract(a, b) {
-	return a - b;
+	return (a - b);
 }
